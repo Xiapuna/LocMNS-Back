d1f2daf300cfefa3b90939de8dca7b16a0cc98a2
Célia <celia.godfrin@gmail.com>
2026-04-13 14:34:55 +0200

    Initial commit

---
+++ src/test/java/com/mns/cda/locmnsback/LocMnsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.cda.locmnsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocMnsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
