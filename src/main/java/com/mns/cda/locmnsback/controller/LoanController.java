f6fbc89deec57be55b05dd1cffd4e1f8706e9441
Célia <celia.godfrin@gmail.com>
2026-04-15 09:02:05 +0200

    FEAT : add Loan model, DAO and controller structure

---
+++ src/main/java/com/mns/cda/locmnsback/controller/LoanController.java
@@ -0,0 +1,75 @@
+package com.mns.cda.locmnsback.controller;
+
+import com.mns.cda.locmnsback.dao.LoanDao;
+import com.mns.cda.locmnsback.model.Loan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class LoanController {
+
+    @Autowired
+    protected LoanDao loanDao;
+
+    @GetMapping("/loan/list")
+    public List<Loan> getAll() {
+        return loanDao.findAll();
+    }
+
+    @GetMapping("/loan/{id}")
+    public ResponseEntity<Loan> get(@PathVariable int id) {
+
+        Optional<Loan> optionalLoan = loanDao.findById(id);
+
+        if(optionalLoan.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(optionalLoan.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/loan")
+    public ResponseEntity<Loan> create(@RequestBody Loan loanToInsert) {
+
+        loanToInsert.setId(null);
+
+        loanDao.save(loanToInsert);
+
+        return new ResponseEntity<>(loanToInsert, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/loan/{id}")
+    public ResponseEntity<Loan> delete(@PathVariable int id) {
+
+        Optional<Loan> optionalLoan = loanDao.findById(id);
+
+        if(optionalLoan.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        loanDao.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/loan/{id}")
+    public ResponseEntity<Void> update(@PathVariable int id, @RequestBody Loan loanToUpdate) {
+
+        Optional<Loan> optionalLoan = loanDao.findById(id);
+
+        if(optionalLoan.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        loanDao.save(loanToUpdate);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
