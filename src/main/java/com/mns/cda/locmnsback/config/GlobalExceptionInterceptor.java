a704d5dccd8eeaa348c9a4ed5820d95a195f118e
Célia <celia.godfrin@gmail.com>
2026-06-08 09:21:31 +0200

    Add GlobalExceptionInterceptor

---
+++ src/main/java/com/mns/cda/locmnsback/config/GlobalExceptionInterceptor.java
@@ -0,0 +1,38 @@
+package com.mns.cda.locmnsback.config;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionInterceptor {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> constraintViolationInterceptor(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldErrors : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fieldErrors.getField(), fieldErrors.getDefaultMessage());
+        }
+
+        return errors;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> getConstraintViolationDatabase(DataIntegrityViolationException ex) {
+
+        return Map.of("Erreur", "Erreur de contrainte dans la base de données");
+
+    }
+
+}
